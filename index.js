d6c7f88d9379e2f6709f1d8624aad5254dff655a
JeffEckert <bromodo@gmail.com>
2020-09-05 09:36:46 -0400

    added first fetch request from api and moved data to newly created html container

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const endPoint = "http://localhost:3000/api/v1/journal_entries"
 
 document.addEventListener('DOMContentLoaded', (event) => {
+   getJournalEntry()
+})
+
+function getJournalEntry() {
     fetch(endPoint)
     .then(response => response.json())
     .then(journal_entries => {
-        console.log(journal_entries);
-    })
-})
+        journal_entries.data.forEach(journal_entry => {
+            const journalEntryMarkup = `
+              <div data-id=${journal_entry.id}>
+                <img src=${journal_entry.attributes.image_url} height="200" width="250">
+                <h3>${journal_entry.attributes.meal}</h3>
+                <p>${journal_entry.attributes.description}</p>
+                <p>${journal_entry.attributes.calorie_count}</p>
+                <p>${journal_entry.attributes.calendar_date.date}</p>
+                <button data-id=${journal_entry.id}>edit</button>
+              </div>
+              <br><br>`;
+    
+              document.querySelector('#journalEntry-container'
+              ).innerHTML += journalEntryMarkup
+          })
+        })
+}
