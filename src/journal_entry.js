d3057a7097b486c05c372eeb1a929c3d6b8dbe54
JeffEckert <bromodo@gmail.com>
2020-09-06 15:38:13 -0400

    built constructior for journalEntry class

--- src/journal_entry.js
+++ src/journal_entry.js
@@ -1,3 +1,12 @@
-class journal_entry {
-
+class JournalEntry {
+    constructor(id, journalEntryAttributes) {
+        this.id = id;
+        this.meal = journalEntryAttributes.meal;
+        this.description = journalEntryAttributes.description;
+        this.image_url = journalEntryAttributes.image_url;
+        this.calorie_count = journalEntryAttributes.calorie_count;
+        this.category = journalEntryAttributes.category;
+        this.date = journalEntryAttributes.date;
+        JournalEntry.call.push(this);
+    }
 }
