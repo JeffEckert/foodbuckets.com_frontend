7c50a3eb9046b16590f41c3e22c30abd7aabadc1
JeffEckert <bromodo@gmail.com>
2020-09-05 21:27:41 -0400

    working on completing post fetchadjusting back end to be able to correct post error

--- index.js
+++ index.js
@@ -37,52 +37,37 @@ function createFormHandler(e) {
     const imageInput = document.querySelector('#input-url').value
     const calorieInput = document.querySelector('#input-calorieCount').value
     const dateInput = document.querySelector('#input-date').value
+    const calendarDateId = parseInt(dateInput)
     postJournalEntry(mealInput, descriptionInput, imageInput, calorieInput, dateInput)
 }
 
-function postJournalEntry(meal, description, image, calorie, date) {
-    console.log(meal, description, image, calorie, date);
+function postJournalEntry(meal, description, image_url, calorie_count, calendar_date_id) {
+    // console.log(meal, description, image_url, calorie_count, calendar_date_id);
 
-    let bodyData = {meal, description, image, calorie, date}
+    let bodyData = {meal, description, image_url, calorie_count, calendar_date_id}
 
     fetch(endPoint, {
-        method: "POST"
+        method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(bodyData)
     })
     .then(response => response.json())
     .then(journal_entry => {
-        console.log(journal_entry);
-        const journalEntryData = journal_entry.data
-        const journalEntryMarkup = `
-        <div data-id=${journal_entry.id}>
-                <img src=${journalEntryData.attributes.image_url} height="200" width="250"></img>
-                <h3>${journal_entry.attributes.meal}</h3>
-                <p>${journal_entry.attributes.description}</p>
-                <p>${journal_entry.attributes.calorie_count}</p>
-                <p>${journal_entry.attributes.calendar_date.date}</p>
-                <button data-id=${journal_entry.id}>edit</button>
-         </div>
-         <br><br>`;
+        console.log(journal_entry)
+        // const journalEntryData = journal_entry.data
+        // const journalEntryMarkup = `
+        // <div data-id=${journal_entry.id}>
+        //         <img src=${journal_entry.attributes.image_url} height="200" width="250">
+        //         <h3>${journal_entry.attributes.meal}</h3>
+        //         <p>${journal_entry.attributes.description}</p>
+        //         <p>${journal_entry.attributes.calorie_count}</p>
+        //         <p>${journal_entry.attributes.calendar_date.date}</p>
+        //         <button data-id=${journal_entry.id}>edit</button>
+        //       </div>
+        //  <br><br>`;
 
-         document.querySelector('#journalEntry-container'
-         ).innerHTML += journalEntryMarkup
+        //  document.querySelector('#journalEntry-container'
+        //  ).innerHTML += journalEntryMarkup
     
     })
-};
-
-function render(journal_entry) {
-    const journalEntryMarkup = `
-        <div data-id=${journal_entry.id}>
-                <img src=${journalEntryData.attributes.image_url} height="200" width="250"></img>
-                <h3>${journal_entry.attributes.meal}</h3>
-                <p>${journal_entry.attributes.description}</p>
-                <p>${journal_entry.attributes.calorie_count}</p>
-                <p>${journal_entry.attributes.calendar_date.date}</p>
-                <button data-id=${journal_entry.id}>edit</button>
-         </div>
-         <br><br>`;
-
-         document.querySelector('#journalEntry-container'
-         ).innerHTML += journalEntryMarkup
-}
+    };
