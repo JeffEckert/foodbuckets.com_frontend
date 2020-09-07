ded4fd45b0a3aae76c4c5634334ce5057076d5a6
JeffEckert <bromodo@gmail.com>
2020-09-07 00:50:28 -0400

    added Bootstrap CSS to my site to make frontend look better

--- src/journal_entry.js
+++ src/journal_entry.js
@@ -9,19 +9,28 @@ class JournalEntry {
         this.category = journalEntryAttributes.category;
         this.date = journalEntryAttributes.date;
         JournalEntry.all.push(this);
+        console.log(this)
     }
     renderJournalEntryCard() {
-        return`
-        <div data-id=${this.id}>
-            <img src=${this.image_url} height="200" width="250">
-            <h3>${this.meal}</h3>
-            <p>${this.description}</p>
-            <p>${this.calorie_count}</p>
-            <p>${this.category.category}</p>
-            <p>${this.date}</p>
-            <button data-id=${this.id}>edit</button>
+       return `
+       <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          <img src="${this.image_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${this.meal}</h5>
+              <p class="card-text">${this.description}</p>
+              <p class="card-text">${this.category.category}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">Calories ${this.calorie_count}</small>
+              </div>
+            </div>
+          </div>
         </div>
-        <br><br>`;
+       `
     }
 }
 
